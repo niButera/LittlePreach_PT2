dccdefea53c597a101bc7fd32e29363bfa2b15a8
NiButera <nikolasxp683@gmail.com>
2023-03-19 00:40:21 -0300

    Realizei o exercicio do pastorzinho adicionando a parte escrita e a parte de beeps

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿#pragma warning disable
+
+Console.WriteLine("Dó, ré, mi, fá, fá, fá,"); 
+
+Thread.Sleep(1000);
+
+Console.Beep(1320, 200); 
+Console.Beep(1485, 200); 
+Console.Beep(1650, 200); 
+Console.Beep(1759, 200); 
+Console.Beep(1759, 200); 
+Console.Beep(1759, 200);
+
+Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
+
+Thread.Sleep(1000);
+
+Console.Beep(1320, 200); 
+Console.Beep(1485, 200); 
+Console.Beep(1320, 200); 
+Console.Beep(1485, 200); 
+Console.Beep(1485, 200); 
+Console.Beep(1485, 200); 
+
+Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+
+Thread.Sleep(1000);
+
+Console.Beep(1320, 200); 
+Console.Beep(1980, 200); 
+Console.Beep(1759, 200); 
+Console.Beep(1650, 200);
+Console.Beep(1650, 200); 
+Console.Beep(1650, 200);
+
+Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+
+Thread.Sleep(1000);
+
+Console.Beep(1320, 200); 
+Console.Beep(1485, 200); 
+Console.Beep(1650, 200); 
+Console.Beep(1759, 200); 
+Console.Beep(1759, 200); 
+Console.Beep(1759, 200); 
